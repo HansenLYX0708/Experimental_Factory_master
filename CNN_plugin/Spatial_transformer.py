3d24d5f2b6c61ba9b1a10bfc037843b364ca9bc6
Hansen Liu <Hansen.Liu@wdc.com>
2021-03-05 15:55:12 +0800

    Add

--- CNN_plugin/Spatial_transformer.py
+++ CNN_plugin/Spatial_transformer.py
@@ -133,6 +133,42 @@ def spatial_yransform_network(x):
     return x_t
 
 
+from utils import get_initial_weights
+from layers import BilinearInterpolation
+from tensorflow.keras.layers import Input, MaxPool2D, Flatten, Dense, Activation
+from tensorflow.keras import  Model
+#####################################################
+##  coding by keras
+#####################################################
+
+def STN(input_shape=(60, 60, 1), sampling_size=(30, 30), num_classes=10):
+    image = Input(shape=input_shape)
+    locnet = MaxPool2D(pool_size=(2, 2))(image)
+    locnet = Conv2D(20, (5, 5))(locnet)
+    locnet = MaxPool2D(pool_size=(2, 2))(locnet)
+    locnet = Conv2D(20, (5, 5))(locnet)
+    locnet = Flatten()(locnet)
+    locnet = Dense(50)(locnet)
+    locnet = Activation('relu')(locnet)
+    weights = get_initial_weights(50)
+    locnet = Dense(6, weights=weights)(locnet)
+    x = BilinearInterpolation(sampling_size)([image, locnet])
+    x = Conv2D(32, (3, 3), padding='same')(x)
+    x = Activation('relu')(x)
+    x = MaxPool2D(pool_size=(2, 2))(x)
+    x = Conv2D(32, (3, 3))(x)
+    x = Activation('relu')(x)
+    x = MaxPool2D(pool_size=(2, 2))(x)
+    x = Flatten()(x)
+    x = Dense(256)(x)
+    x = Activation('relu')(x)
+    x = Dense(num_classes)(x)
+    x = Activation('softmax')(x)
+    return Model(inputs=image, outputs=x)
+
+
+
+
 if __name__ == '__main__':
     model = SpatialTransformer()
     #model.build(input_shape=(None, 32, 32, 1))
